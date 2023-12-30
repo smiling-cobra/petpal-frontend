96979455fad91dfbb5f7a3fd5ee9fdaaada5f688
Jenya Golovnov <93ave.blues@gmail.com>
2023-12-31 00:12:08 +0100

    added tailwind / updated basic register page markup

--- src/components/templates/Register.svelte
+++ src/components/templates/Register.svelte
@@ -4,14 +4,28 @@
 
   const handleLogin = async () => {
     // Implement login logic here
-    // For example, sending credentials to a server
     console.log('Login attempt with', email, password);
     // On successful login, redirect to another route or set user session
   };
 </script>
 
-<form on:submit|preventDefault={handleLogin}>
-  <input type="email" bind:value={email} placeholder="Email" />
-  <input type="password" bind:value={password} placeholder="Password" />
-  <button type="submit">Login</button>
+<form on:submit|preventDefault={handleLogin} class="flex flex-col gap-4 p-4 max-w-sm mx-auto">
+  <input
+    type="email"
+    bind:value={email}
+    placeholder="Email"
+    class="px-4 py-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none transition duration-200"
+  />
+  <input
+    type="password"
+    bind:value={password}
+    placeholder="Password"
+    class="px-4 py-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none transition duration-200"
+  />
+  <button
+    type="submit"
+    class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 transition duration-200"
+  >
+    Login
+  </button>
 </form>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.svelte', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
